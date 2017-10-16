373c42f21e9a70c933b6bbb5a72086f9a937b40f
lwr <ran0046@foxmail.com>
2017-10-16 17:49:56 +0800

    update

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+var questions=[
+    {
+        data:213,
+        num:444,
+        age:12
+    },
+    {
+        data:456,
+        num:678,
+        age:13
+    }];
+
+app.get('/123', function(req, res) {
+   res.status(200);
+   res.json(questions);
+});
