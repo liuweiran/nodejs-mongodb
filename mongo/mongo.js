8fcf3d0db3f90fd956faed7c06d244cfc2451968
lwr <ran0046@foxmail.com>
2017-10-27 17:48:59 +0800

    update

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -8,7 +8,7 @@ function init() {
     mongodb.MongoClient // 获得mongodb客户端对象
         .connect("mongodb://localhost:5000/demo") // 使用客户端连接数据  mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
         .then( db => {
-            logger.info("connect");
+            logger.info('connect success');
             globalDb = db;
         })
         .catch( err => {
